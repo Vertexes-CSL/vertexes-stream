c560e5ed9cd4e8ecfaf883cf9b6b8a0d0e3648b8
ernestbenedictus <m010d4ky1623@bangkit.academy>
2024-11-12 21:57:51 +0700

    uploads weather data to mongodb

--- weather_source/produce/celery.py
+++ weather_source/produce/celery.py
@@ -1,7 +1,7 @@
 from celery import Celery
 from produce.fetch_weather import fetch_weather_data_srg, fetch_weather_data_jkt, fetch_weather_data_bdg, fetch_weather_data_smr, fetch_weather_data_sby
-# from produce.kafka_producer import send_to_kafka
 from datetime import datetime
+from pymongo import MongoClient
 import json
 import os
 
@@ -12,37 +12,48 @@ celery_app.autodiscover_tasks([])
 
 celery_app.conf.broker_connection_retry_on_startup = True
 
+client = MongoClient(os.getenv("MONGODB"))
+
+db = client.openweather_db
+
+collection = db.weather_records
+
 celery_app.conf.beat_schedule = {
-    "fetch-weather-srg-every-30-seconds": {
+    "fetch-weather-srg-every-5-minutes": {
         "task": "produce.celery.scheduled_fetch_and_save_srg",
-        "schedule": 15.0,  
+        "schedule": 300.0,  
     },
-    "fetch-weather-jkt-every-30-seconds": {
+    "fetch-weather-jkt-every-5-minutes": {
         "task": "produce.celery.scheduled_fetch_and_save_jkt",
-        "schedule": 15.0,  
+        "schedule": 300.0,  
     },
-    "fetch-weather-bdg-every-30-seconds": {
+    "fetch-weather-bdg-every-5-minutes": {
         "task": "produce.celery.scheduled_fetch_and_save_bdg",
-        "schedule": 15.0,  
+        "schedule": 300.0,  
     },
-    "fetch-weather-smr-every-30-seconds": {
+    "fetch-weather-smr-every-5-minutes": {
         "task": "produce.celery.scheduled_fetch_and_save_smr",
-        "schedule": 15.0,  
+        "schedule": 300.0,  
     },
-    "fetch-weather-sby-every-30-seconds": {
+    "fetch-weather-sby-every-5-seconminutesds": {
         "task": "produce.celery.scheduled_fetch_and_save_sby",
-        "schedule": 15.0,  
+        "schedule": 300.0,  
     },
 }
+
+def save_to_mongodb(data):
+    try:
+        collection.insert_one(data)
+        print("Data inserted successfully")
+    except Exception as e:
+        print(f"Error inserting data into MongoDB: {e}")
 
 @celery_app.task()
 def scheduled_fetch_and_save_srg():
     weather_data = fetch_weather_data_srg()
     if weather_data:
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        filename = os.path.join("/app/output", "output_srg", f"weather_data_srg_{timestamp}.json")
-        with open(filename, "w") as file:
-            json.dump(weather_data, file)
+        weather_data["timestamp"] = datetime.now().isoformat()
+        save_to_mongodb(weather_data)
     else:
         print("Failed to fetch Serang weather data")
 
@@ -50,10 +61,8 @@ def scheduled_fetch_and_save_srg():
 def scheduled_fetch_and_save_jkt():
     weather_data = fetch_weather_data_jkt()
     if weather_data:
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        filename = os.path.join("/app/output", "output_jkt", f"weather_data_bdg_{timestamp}.json")
-        with open(filename, "w") as file:
-            json.dump(weather_data, file)
+        weather_data["timestamp"] = datetime.now().isoformat()
+        save_to_mongodb(weather_data)
     else:
         print("Failed to fetch Jakarta weather data")
 
@@ -61,10 +70,8 @@ def scheduled_fetch_and_save_jkt():
 def scheduled_fetch_and_save_bdg():
     weather_data = fetch_weather_data_bdg()
     if weather_data:
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        filename = os.path.join("/app/output", "output_bdg", f"weather_data_bdg_{timestamp}.json")
-        with open(filename, "w") as file:
-            json.dump(weather_data, file)
+        weather_data["timestamp"] = datetime.now().isoformat()
+        save_to_mongodb(weather_data)
     else:
         print("Failed to fetch Bandung weather data")
 
@@ -72,10 +79,8 @@ def scheduled_fetch_and_save_bdg():
 def scheduled_fetch_and_save_smr():
     weather_data = fetch_weather_data_smr()
     if weather_data:
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        filename = os.path.join("/app/output", "output_smr", f"weather_data_smr_{timestamp}.json")
-        with open(filename, "w") as file:
-            json.dump(weather_data, file)
+        weather_data["timestamp"] = datetime.now().isoformat()
+        save_to_mongodb(weather_data)
     else:
         print("Failed to fetch Semarang weather data")
 
@@ -83,54 +88,7 @@ def scheduled_fetch_and_save_smr():
 def scheduled_fetch_and_save_sby():
     weather_data = fetch_weather_data_sby()
     if weather_data:
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        filename = os.path.join("/app/output", "output_sby", f"weather_data_sby_{timestamp}.json")
-        with open(filename, "w") as file:
-            json.dump(weather_data, file)
+        weather_data["timestamp"] = datetime.now().isoformat()
+        save_to_mongodb(weather_data)
     else:
         print("Failed to fetch Surabaya weather data")
-
-
-# @celery_app.task()
-# def scheduled_fetch_and_save_srg():
-#     weather_data = fetch_weather_data_srg()
-#     if weather_data:
-#         send_to_kafka(weather_data)
-#     else:
-#         print("Failed to fetch Serang weather data")
-
-# @celery_app.task()
-# def scheduled_fetch_and_save_jkt():
-#     weather_data = fetch_weather_data_jkt()
-#     if weather_data:
-#         send_to_kafka(weather_data)
-
-#     else:
-#         print("Failed to fetch Jakarta weather data")
-
-# @celery_app.task()
-# def scheduled_fetch_and_save_bdg():
-#     weather_data = fetch_weather_data_bdg()
-#     if weather_data:
-#         send_to_kafka(weather_data)
-
-#     else:
-#         print("Failed to fetch Bandung weather data")
-
-# @celery_app.task()
-# def scheduled_fetch_and_save_smr():
-#     weather_data = fetch_weather_data_smr()
-#     if weather_data:
-#         send_to_kafka(weather_data)
-
-#     else:
-#         print("Failed to fetch Semarang weather data")
-
-# @celery_app.task()
-# def scheduled_fetch_and_save_sby():
-#     weather_data = fetch_weather_data_sby()
-#     if weather_data:
-#         send_to_kafka(weather_data)
-#     else:
-#         print("Failed to fetch Surabaya weather data")
-
